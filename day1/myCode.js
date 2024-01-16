fc2d3b833e6f1fc9255e1d76f9f43649a85a8c1f
Umakantk8090 <uk8418805147@gmail.com>
2024-01-16 19:26:43 +0530

    mycode

---
+++ day1/myCode.js
@@ -0,0 +1,27 @@
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************Operation   *******
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2=" umakant"
+let str=str1+str2;
+console.log(str);
+
